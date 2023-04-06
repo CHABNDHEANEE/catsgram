022e184aad5cb72fc4aae5d9c500ffbc5d42d07e
Daniil <rusbear060@gmail.com>
2023-04-06 21:30:02 +0300

    add getpostbyid endpoint

--- src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
@@ -8,6 +8,7 @@ import ru.yandex.practicum.catsgram.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostService {
@@ -21,6 +22,12 @@ public class PostService {
 
     public List<Post> findAll() {
         return posts;
+    }
+
+    public Optional<Post> findPostById(int postId) {
+        return posts.stream()
+                .filter(x -> x.getId() == postId)
+                .findFirst();
     }
 
     public Post create(Post post) {
